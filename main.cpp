33245f54c7043a2a36ca95bdb3d1e61c240e5fd3
Santiago Palomares <spaloma.v1@gmail.com>
2024-07-01 14:59:06 -0700

    opening camrea

--- main.cpp
+++ main.cpp
@@ -1,48 +1,20 @@
-#include <opencv2/opencv.hpp>
-#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#
 
-int main() {
-    // Path to the video file
-    std::string videoPath = "resources/converted_video.mp4";
-    
-    // Open the video file
-    cv::VideoCapture cap(videoPath);
-    
-    // Check if the video opened successfully
-    if (!cap.isOpened()) {
-        std::cerr << "Error opening video file: " << videoPath << std::endl;
-        return -1;
+using namespace std;
+using namespace cv;
+
+int main(){
+    VideoCapture cap(1);
+    Mat img;
+
+    while (true)
+    {
+        cap.read(img);
+        imshow("Image", img);
+        waitKey(1);
     }
-
-    // Get the frames per second of the video
-    double fps = cap.get(cv::CAP_PROP_FPS);
-
-    // Create a window for display
-    cv::namedWindow("Video", cv::WINDOW_NORMAL);
-
-    while (true) {
-        cv::Mat frame;
-        
-        // Capture frame-by-frame
-        cap >> frame;
-
-        // If the frame is empty, break the loop
-        if (frame.empty())
-            break;
-
-        // Display the resulting frame
-        cv::imshow("Video", frame);
-
-        // Press  'q' to exit the loop
-        if (cv::waitKey(1000 / fps) == 'q')
-            break;
-    }
-
-    // When everything is done, release the video capture object
-    cap.release();
-
-    // Close all OpenCV windows
-    cv::destroyAllWindows();
-
     return 0;
 }
