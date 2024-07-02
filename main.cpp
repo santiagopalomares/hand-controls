3fa7e41dfa90d14dba08eb83c6649b7f54cbcb97
Santiago Palomares <spaloma.v1@gmail.com>
2024-07-01 20:39:30 -0700

    displaying edges

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
-#
+#include <opencv2/videoio.hpp>
+#include <iostream> // Include this for cerr
 
 using namespace std;
 using namespace cv;
 
 int main(){
-    VideoCapture cap(1);
-    Mat img;
+    VideoCapture cap(0); // Use 0 for the default camera
+    if (!cap.isOpened()) {
+        cerr << "Error: Could not open camera" << endl;
+        return -1;
+    }
+
+    Mat img, imgBlur, imgCanny;
 
     while (true)
     {
         cap.read(img);
-        imshow("Image", img);
-        waitKey(1);
+        if (img.empty()) {
+            cerr << "Error: Could not grab frame" << endl;
+            break;
+        }
+
+        GaussianBlur(img, imgBlur, Size(3, 3), 3, 0);
+        Canny(imgBlur, imgCanny, 50, 150);
+        imshow("Image", imgCanny);
+
+        if (waitKey(1) >= 0) {
+            break;
+        }
     }
     return 0;
 }
